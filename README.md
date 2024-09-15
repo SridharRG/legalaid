3ee159cf6dc261ed9f3dd98d9e8efa4b13232371
SridharRG <sridhar79.rgss@gmail.com>
2024-09-15 17:00:35 +0530

    updated legalaid model and api endpoints

--- README.md
+++ README.md
@@ -1,17 +1,56 @@
-# Legal-AId
+# Legal-Aid
+
+![Home Page](./doc/main-page.png) Home Page 
+![ChatBot](./doc/chatbot.png) Chat Bot
+
+## Languages, Frameworks, Tools used
+
+- Astro.js framework
+- astro integration with React
+- Tailwind CSS for styling
+- Python
+- Mistral (Hugging Face)
+- AI framework - LangChain
 
 
+## Build, test and run
 
-![just-the-basics](https://github.com/withastro/astro/assets/2244813/a0a5533c-a856-4198-8470-2d67b1d7c554)
+- Clone the repository
+```shell
+$ git clone https://github.com/SridharRG/legalaid.git
+```
+- Install all dependencies
+
+```shell
+$ pip install -r requirements.txt
+```
+
+```shell
+$ npm i
+```
+- Run the server
+    ```shell
+    $ npm start
+    ```
+
+    The application will be running at `http://localhost:4321`
+
+- 
+    - Running the Backend
+    ```shell
+    $ uvicorn legal_model:app --host 0.0.0.0 --port 8000 --reload
+    ```
+    
+    This will start the FastAPI server at http://0.0.0.0:8000/api/legal-assist/.
 
 ## 🚀 Project `Astro.js` Snippets
 
 
-Astro looks for `.astro` or `.md` files in the `src/pages/` directory. Each page is exposed as a route based on its file name.
+- Astro looks for `.astro` or `.tsx` files in the `src/pages/` directory. Each page is exposed as a route based on its file name.
 
-There's nothing special about `src/components/`, but that's where we like to put any Astro/React/Vue/Svelte/Preact components.
+- There's nothing special about `src/components/`, but that's where we like to put any Astro/React components.
 
-Any static assets, like images, can be placed in the `public/` directory.
+- Any static assets, like images, can be placed in the `public/` directory.
 
 
 ## 🧞 Commands
--- backend/models/legal_model.py
+++ backend/models/legal_model.py
@@ -1,8 +1,76 @@
-class LegalModel:
-    def __init__(self):
-        # model
-        pass
+ 
+import os
+import torch
+from operator import itemgetter
+from transformers import BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.vectorstores import Qdrant
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.prompts import ChatMessagePromptTemplate, PromptTemplate
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.embeddings import HuggingFaceBgeEmbeddings
 
-    def get_advice(self, question):
-        # question and return legal advice
-        return "This is a placeholder response."
+device = torch.device('cpu')
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+# Load model components
+model = AutoModelForCausalLM.from_pretrained('/home/aani/Downloads/Colab Notebooks/mistral_model')
+tokenizer = AutoTokenizer.from_pretrained('/home/aani/Downloads/Colab Notebooks/mistral_tokenizer')
+
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    use_cache=True,
+    device_map=-1,
+    max_new_tokens=5000,
+    do_sample=True,
+    top_k=3,
+    temperature=0.01,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+model_pipeline = HuggingFacePipeline(pipeline=pipe)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Load the retriever
+with open('/content/drive/MyDrive/Colab Notebooks/qdrant_retriever.pkl', 'rb') as f:
+    qdrant_retriever = pickle.load(f)
+
+# Load the prompt template
+with open('/content/drive/MyDrive/Colab Notebooks/chat_prompt_template.txt', 'r') as f:
+    loaded_template = f.read()
+
+prompt = ChatPromptTemplate.from_template(loaded_template)
+setup_and_retrieval = RunnableParallel(
+    {"context": qdrant_retriever | format_docs, "question": RunnablePassthrough()}
+)
+rag_chain = setup_and_retrieval | prompt | model_pipeline | StrOutputParser()
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.json
+    user_query = data.get('query')
+    user_query ="Cite me a dispute related to electricity board tender"
+    if user_query:
+        result = rag_chain.invoke(user_query)
+        print("anss!!!!!!!!!!!!!!---------------------------------",result)
+        return jsonify({'response': result})
+    return jsonify({'error': 'No query provided'}), 400
+
+if __name__ == '__main__':
+    query()
+    app.run(debug=True)
+
